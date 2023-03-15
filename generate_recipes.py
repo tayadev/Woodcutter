8c1046eddf404691fe0c268f9f5f00fb7d3bc5b2
Taya <taya@taya.one>
2023-03-15 10:26:29 +0100

    moved config to a seperate file

--- generate_recipes.py
+++ generate_recipes.py
@@ -1,21 +1,8 @@
 from beet import Context, Recipe
+import yaml
 
-woodtypes = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'crimson', 'warped', 'bamboo', 'cherry']
-objects = {
-    'stairs': 1,
-    'slab': 2,
-    'fence': 1,
-    'fence_gate': 1,
-    'door': 1,
-    'trapdoor': 1,
-    'pressure_plate': 1,
-    'button': 1,
-    'sign': 1,
-    'hanging_sign': 1
-}
-
-def isNetherWood(wood):
-  return wood in ['crimson', 'warped']
+with open('config.yml', 'r') as file:
+  config = yaml.safe_load(file)
 
 def create_recipe(ctx, input, output, count):
   ctx.data[f'woodcutter:{output}'] = Recipe({
@@ -26,21 +13,34 @@ def create_recipe(ctx, input, output, count):
   })
 
 def beet_default(ctx: Context):
-  for woodtype in woodtypes:
+  for wood in config["types"]:
+    if type(wood) is str:
+      wood = {
+        'name': wood,
+        'type': 'normal'
+      }
+
     # log -> wood
     create_recipe(
       ctx,
-      f'{woodtype}_{isNetherWood(woodtype) and "stem" or "log"}',
-      f'{woodtype}_{isNetherWood(woodtype) and "hyphae" or "wood"}',
-      1
+      f'{wood["name"]}_{wood["type"] == "nether" and "stem" or "log"}',
+      f'{wood["name"]}_{wood["type"] == "nether" "hyphae" or "wood"}',
+      config["counts"]["wood"]
     )
+
     # stripped_log -> stripped_wood
     create_recipe(
       ctx,
-      f'stripped_{woodtype}_{isNetherWood(woodtype) and "stem" or "log"}',
-      f'stripped_{woodtype}_{isNetherWood(woodtype) and "hyphae" or "wood"}',
-      1
+      f'stripped_{wood["name"]}_{wood["type"] == "nether" and "stem" or "log"}',
+      f'stripped_{wood["name"]}_{wood["type"] == "nether" and "hyphae" or "wood"}',
+      config["counts"]["stripped_wood"]
     )
-    # objects
-    for obj, count in objects.items():
-      create_recipe(ctx, f'{woodtype}_planks', f'{woodtype}_{obj}', count)
+
+    # items
+    for item in config["items"]:
+      if type(item) is str:
+        item = {
+          'name': item,
+          'count': 1
+        }
+      create_recipe(ctx, f'{wood["name"]}_planks', f'{wood["name"]}_{item["name"]}', item["count"])
